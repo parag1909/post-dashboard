05e956110840af39fa783ade4861aa4c31d3ca2a
Parag Wayangankar <parag.wayangankar@merkle.com>
2025-02-13 00:35:37 +0530

    deployment bug fix

--- backend/config/logger.js
+++ backend/config/logger.js
@@ -1,16 +1,12 @@
 import winston from "winston";
-import "winston-daily-rotate-file";
-import path from "path";
-
-const logDirectory = path.join(process.cwd(), "logs");
-
+ 
 const logFormat = winston.format.combine(
   winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
   winston.format.errors({ stack: true }),
   winston.format.splat(),
   winston.format.json()
 );
-
+ 
 const logger = winston.createLogger({
   level: "info",
   format: logFormat,
@@ -20,24 +16,8 @@ const logger = winston.createLogger({
         winston.format.colorize(),
         winston.format.simple()
       ),
-    }),
-
-    new winston.transports.DailyRotateFile({
-      filename: path.join(logDirectory, "error-%DATE%.log"),
-      datePattern: "YYYY-MM-DD",
-      level: "error",
-      maxSize: "20m",
-      maxFiles: "14d",
-    }),
-
-    new winston.transports.DailyRotateFile({
-      filename: path.join(logDirectory, "combined-%DATE%.log"),
-      datePattern: "YYYY-MM-DD",
-      level: "info",
-      maxSize: "20m",
-      maxFiles: "14d",
-    }),
-  ],
+    })
+  ]
 });
-
+ 
 export default logger;
--- frontend/post-board-app/src/context/AppContext.jsx
+++ frontend/post-board-app/src/context/AppContext.jsx
@@ -5,8 +5,8 @@ import "react-toastify/dist/ReactToastify.css";
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
-  // const backendURL = import.meta.env.VITE_BACKEND_URL;
-  const backendURL = `http://localhost:8080`;
+  const backendURL = import.meta.env.VITE_BACKEND_URL;
+  // const backendURL = `http://localhost:8080`;
 
   const [isLoading, setIsLoading] = useState(false);
   const [token, setToken] = useState(localStorage.getItem("token") || null);
